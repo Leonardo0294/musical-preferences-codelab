509067837f771e17f239219edd7b22a159721ac1
Leonardo0294 <chichelezcano94@hotmail.com.ar>
2024-04-27 23:20:49 -0300

    segundo commit

--- Gustos Musicales/app.js
+++ Gustos Musicales/app.js
@@ -1,87 +1,122 @@
-const form = document.getElementById('formulario').addEventListener('submit', (event) => {
-    event.preventDefault()
+import { getTopKRecommendations } from "./rc.js";
 
-    console.log(Array.from(event.target));
+const form = document.querySelector("form");
+const output = document.querySelector("#recommendations");
 
-})
+const bands = [
+  "Soda Stereo",
+  "Café Tacvba",
+  "Héroes del Silencio",
+  "Maná",
+  "Los Fabulosos Cadillacs",
+  "Molotov",
+  "La Ley",
+  "Enanitos Verdes",
+  "Caifanes",
+  "Los Prisioneros",
+];
 
-// Tidy to auto-clean all these tensors
-tf.tidy(() => {
-    const users = ['Gant', 'Todd', 'Jed', 'Justin']
-    const bands = [
-      'Nirvana',
-      'Nine Inch Nails',
-      'Backstreet Boys',
-      'N Sync',
-      'Night Club',
-      'Apashe',
-      'STP',
-    ]
-    const features = [
-      'Grunge',
-      'Rock',
-      'Industrial',
-      'Boy Band',
-      'Dance',
-      'Techno',
-    ]
+const features = [
+  "Rock",
+  "Metal",
+  "Pop",
+  "Hard Rock",
+  "Grunge",
+  "Psicodélico",
+  "Alternativo",
+  "Indie",
+  "Clásico",
+];
 
-    // User votes <4>
-    const user_votes = tf.tensor([
-      [10, 9, 1, 1, 8, 7, 8],
-      [6, 8, 2, 2, 0, 10, 0],
-      [0, 2, 10, 9, 3, 7, 0],
-      [7, 4, 2, 3, 6, 5, 5],
-    ])
+const band_feats = [
+  [5, 0, 4, 0, 0, 0, 0, 0, 3],
+  [4, 0, 0, 5, 0, 0, 0, 0, 3],
+  [4, 0, 0, 0, 0, 5, 0, 0, 3],
+  [5, 0, 4, 4, 0, 0, 0, 0, 4],
+  [5, 0, 0, 4, 0, 0, 0, 0, 4],
+  [5, 0, 4, 4, 5, 0, 4, 0, 0],
+  [5, 0, 0, 4, 0, 5, 0, 0, 4],
+  [5, 0, 4, 0, 0, 0, 4, 4, 0],
+  [5, 3, 0, 0, 0, 0, 0, 0, 0],
+  [5, 0, 4, 0, 0, 0, 0, 0, 4],
+];
 
-    // Music Styles <5>
-    const band_feats = tf.tensor([
-      [1, 1, 0, 0, 0, 0],
-      [1, 0, 1, 0, 0, 0],
-      [0, 0, 0, 1, 1, 0],
-      [0, 0, 0, 1, 0, 0],
-      [0, 0, 1, 0, 0, 1],
-      [0, 0, 1, 0, 0, 1],
-      [1, 1, 0, 0, 0, 0],
-    ])
+document.addEventListener("DOMContentLoaded", () => {
+  for (const band of bands) {
+    const div = document.createElement("div");
+    div.classList.add("band");
+    div.innerHTML = `
+      <h6>${band}</h6>
+      <div class="d-flex gap-3">
+        ${new Array(features.length)
+          .fill()
+          .map((_, index) => {
+            return `
+              <div class="form-check">
+                <input class="form-check-input" type="radio" data-value="${
+                  index + 1
+                }" name="${band}" id="${band}-${index + 1}">
+                <label class="form-check-label" for="${band}-${index + 1}">
+                  ${index + 1}
+                </label>
+              </div>
+            `;
+          })
+          .join("")}
+      </div>
+    `;
+    form.appendChild(div);
+  }
 
-    // User's favorite styles
-    const user_feats = tf.matMul(user_votes, band_feats)
-    // Print the answers
-    user_feats.print()
+  const submitButton = document.createElement("button");
+  submitButton.classList.add("btn", "btn-primary", "w-100");
+  submitButton.textContent = "Enviar";
+  form.appendChild(submitButton);
 
-    // Let's make them pretty
-    const top_user_features = tf.topk(user_feats, features.length)
-    // Back to JavaScript
-    const top_genres = top_user_features.indices.arraySync()
-    // print the results
-    users.map((u, i) => {
-      const rankedCategories = top_genres[i].map((v) => features[v])
-      console.log(u, rankedCategories)
-    })
-  })
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const user_votes = getUserVotes();
+    const [{ recommendations }] = getTopKRecommendations(
+      user_votes,
+      band_feats,
+      features.length,
+      features
+    );
+    renderRecommendations(recommendations);
+  });
+});
 
-tf.tidy(() => {
-    const bands = [
-        "Nirvana",
-        "Cuarteto de Nos",
-        "Soda Stereo",
-        "Coldplay",
-        "Ed Sheeran",
-        "Fall out boys",
-        "Skillet",
-        "Shakira",
-        "Maná",
-    ]
+function renderRecommendations(recommendations) {
+  output.innerHTML = ""; // Limpiar recomendaciones anteriores
+  const div = document.createElement("ol");
+  div.classList.add("recommendations");
+  const categories = ["gold", "silver", "bronze"];
+  recommendations.forEach((recommendation, index) => {
+    const className = categories[index] ?? "bg-info-subtle";
+    const li = document.createElement("li");
+    li.classList.add("recommendation", className, "d-flex", "justify-content-start", "align-items-center", "gap-2");
+    li.innerHTML = `
+      <p>${index + 1}</p>
+      <div class="d-flex w-100 justify-content-between align-items-center">
+        <p>${recommendation.feature}</p>
+        <p>${recommendation.value}</p>
+      </div>
+    `;
+    div.appendChild(li);
+  });
+  output.appendChild(div);
+}
 
-    const features = [
-        "Grunge",
-        "Rock",
-        "Punk",
-        "Pop",
-        "Folk",
-        "Metal",
-        "Alternativo",
-        "Latino"
-    ]
-})
+function getUserVotes() {
+  const user_votes = [];
+  for (const band of bands) {
+    const radios = document.querySelectorAll(`input[name="${band}"]:checked`);
+    if (radios.length > 0) {
+      const value = parseInt(radios[0].getAttribute("data-value"));
+      user_votes.push(value);
+    } else {
+      user_votes.push(0); // Si no se ha seleccionado ningún botón de radio, asignar 0 como voto predeterminado
+    }
+  }
+  return user_votes;
+}
---
+++ Gustos Musicales/rc.js
@@ -0,0 +1,22 @@
+export function getTopKRecommendations(user_votes, band_feats, k, features) {
+    return tf.tidy(() => {
+      const user_votes_tensor = tf.tensor([user_votes]);
+      const band_feats_tensor = tf.tensor(band_feats);
+      const user_feats = tf.matMul(user_votes_tensor, band_feats_tensor);
+      const { indices, values } = user_feats.topk(k);
+      const arr = values.arraySync();
+      return indices.arraySync().map((indices, userIndex) => {
+        console.log("indices: ", indices);
+        console.log("values: ", arr);
+        return {
+          user: "Yo",
+          recommendations: indices.map((index, i) => {
+            return {
+              feature: features[index],
+              value: arr[userIndex][i],
+            };
+          }),
+        };
+      });
+    });
+  }
